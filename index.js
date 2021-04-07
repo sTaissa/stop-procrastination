527a71f87977a3195ec153a9a483d55b815111a3
sTaissa <staissa002@gmail.com>
2021-04-07 19:45:21 -0300

    displays a message when loading facebook

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-actived.addEventListener("click", async () => {
-    activ = document.getElementById("actived");
-    if (activ.value == 'false'){
-        activ.value = 'true';
-        alert("true");
-    }
-    else 
-        activ.value = 'false';
-})
+$(document).ready(function() {
+    alert("Don't waste time with this, do something more productive!");
+});
