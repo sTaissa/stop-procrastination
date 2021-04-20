577021253f9de52e71f1b40d4d59167c5ab65921
sTaissa <staissa002@gmail.com>
2021-04-20 19:41:20 -0300

    observes when the button is modified

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
-    alert("Don't waste time with this, do something more productive!");
-});
+butActived.addEventListener("click", function(){
+    if (butActived.value == 'true') {
+        butActived.value = false;
+    } else {
+        butActived.value = true;
+    }
+})
+
+//dispara changeButton sempre que o valor deste é alterado
+var observer = new MutationObserver( changeButton);
+var config = {attributes: true }; //mudanças a serem oservadas(atributos no caso)
+  
+function changeButton(mutations) {
+    console.log( mutations );
+}
+  
+observer.observe(butActived, config);
