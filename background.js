575f985bb21c9ddf9abd1cd9765574e76b51576b
sTaissa <staissa002@gmail.com>
2021-05-05 16:34:23 -0300

    takes the url of the current page

--- background.js
+++ background.js
@@ -21,3 +21,14 @@ chrome.action.onClicked.addListener((tab) => {
 function message() {
     alert("Don't waste time with this, do something more productive!");
 }
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   console.log(changeInfo.url + " aa");
+}); 
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  // how to fetch tab url using activeInfo.tabid
+  chrome.tabs.get(activeInfo.tabId, function(tab){
+     console.log(tab.url);
+  });
+});
