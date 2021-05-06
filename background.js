f68af56e5401bead5538e38a3449187d4e267d5e
sTaissa <staissa002@gmail.com>
2021-05-05 21:29:04 -0300

    displays a message when loading one of the list links
    the best way so far

--- background.js
+++ background.js
@@ -1,22 +1,16 @@
 //list of sites that user should be "warned"
 var sites = ["facebook", "instagram"];
 
-//when the extension is clicked
-chrome.action.onClicked.addListener((tab) => {
-    //check if the current tab is part of the list of sites
+//check if the current tab is part of the list of sites
+function match(url){
     for (i in sites) {
-        if (tab.url.includes(sites[i])) {
-            //if true, run the scipt that displays an alert
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id}, 
-                function: message
-            });
+        if (url.includes(sites[i])) {
+            //if true, run the script that displays an alert
+            console.log("achou");
             break;
-        } else {
-            console.log("não achou, so pra controle");
-        }
+        } 
     } 
-});
+}
 
 function message() {
     alert("Don't waste time with this, do something more productive!");
@@ -24,11 +18,16 @@ function message() {
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
    console.log(changeInfo.url + " aa");
+   if (changeInfo.url != undefined) {
+        match(changeInfo.url);
+   }
 }); 
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  // how to fetch tab url using activeInfo.tabid
   chrome.tabs.get(activeInfo.tabId, function(tab){
      console.log(tab.url);
+     if (tab.url != undefined) {
+        match(tab.url);
+     }
   });
 });
