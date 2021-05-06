901107f778415b576dd16ae1b43a19201a547f5a
sTaissa <staissa002@gmail.com>
2021-05-05 22:50:31 -0300

    performs the functions only if the button is activated

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 //list of sites that user should be "warned"
 var sites = ["facebook", "instagram"];
 
-//check if the current tab is part of the list of sites
+//set the button in "idle mode" initially
+chrome.storage.sync.set({activ: false});
+
+//function to check if the current tab is part of the list of sites
 function match(url){
     for (i in sites) {
         if (url.includes(sites[i])) {
@@ -16,18 +19,34 @@ function message() {
     alert("Don't waste time with this, do something more productive!");
 }
 
+//when current tab is updated
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-   console.log(changeInfo.url + " aa");
-   if (changeInfo.url != undefined) {
-        match(changeInfo.url);
-   }
+  //get the button mode
+  chrome.storage.sync.get(['activ'], function(result) {
+    //if its active and tab has a url, call match function
+    if (result.activ == true && changeInfo.url != undefined) {
+      match(changeInfo.url);
+    }
+  });
 }); 
 
+//when a tab is activated
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+  //get the info(inclunding url) of activated tab
   chrome.tabs.get(activeInfo.tabId, function(tab){
-     console.log(tab.url);
-     if (tab.url != undefined) {
+    //get the button mode
+    chrome.storage.sync.get(['activ'], function(result) {
+      //if its active and tab has a url, call match function
+      if (result.activ == true && tab.url != undefined) {
         match(tab.url);
-     }
+      }
+    });
   });
+});
+
+//changes the button mode everytime it is clicked
+chrome.storage.onChanged.addListener(function (changes) {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    chrome.storage.sync.set({activ: newValue});
+  }
 });
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-chrome.storage.sync.set({activ: false});
-
-actived.addEventListener("click", async () => {
+butActived.addEventListener("click", async () => {
     chrome.storage.sync.get(['activ'], function(result) {
         if (result.activ == false){
             chrome.storage.sync.set({activ: true});
+            console.log(result.activ);
         } else {
             chrome.storage.sync.set({activ: false});
+            console.log(result.activ);
         }
     });
 })
